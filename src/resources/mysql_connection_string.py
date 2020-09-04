d1b38018d2b258f5b345bcfec1931a9779af8c8f
Lysenko Viktor <lysenko_va@groupbwt.com>
2020-09-05 00:53:52 +0300

    change function

--- src/resources/mysql_connection_string.py
+++ src/resources/mysql_connection_string.py
@@ -3,20 +3,10 @@ from os import getenv
 from dotenv import load_dotenv
 
 
-def mysql_connection_string_for_app() -> str:
+def mysql_connection_string(enable_driver: bool = False) -> str:
     load_dotenv()
-    return "mysql://{}:{}@{}:{}/{}".format(
-        getenv("MYSQL_USER"),
-        getenv("MYSQL_PASSWORD"),
-        getenv("MYSQL_HOST"),
-        getenv("MYSQL_PORT"),
-        getenv("MYSQL_DATABASE")
-    )
-
-
-def mysql_connection_string_for_alembic() -> str:
-    load_dotenv()
-    return "mysql+pymysql://{}:{}@{}:{}/{}".format(
+    return "mysql{}://{}:{}@{}:{}/{}".format(
+        '+pymysql' if enable_driver else '',
         getenv("MYSQL_USER"),
         getenv("MYSQL_PASSWORD"),
         getenv("MYSQL_HOST"),
