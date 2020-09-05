619b06488e59ac05966a8ee31509d07d0c298c6c
Lysenko Viktor <lysenko_va@groupbwt.com>
2020-09-05 23:15:08 +0300

    add methods

--- src/database/repositories/user_repository.py
+++ src/database/repositories/user_repository.py
@@ -1,16 +1,17 @@
-from sqlalchemy import insert
+from loguru import logger
+from sqlalchemy import insert, select
 from pymysql.err import IntegrityError
 
 from .base_repository import BaseRepository
-from .errors import EntityAlreadyExist
+from .errors import EntityAlreadyExist, EntityDoesNotExist, FailedCredentials
 from database.models import Users
-from schemas import UserInCreate
-from services import security
+from schemas import UserInCreate, UserInLogin
+from services import security, create_access_token_for_user
 
 
 class UserRepository(BaseRepository):
 
-    async def create_user(self, user_create: UserInCreate):
+    async def create_user(self, user_create: UserInCreate) -> None:
         try:
             await self.database.execute(
                 insert(Users).values(
@@ -19,4 +20,31 @@ class UserRepository(BaseRepository):
                     password_hash=security.get_password_hash(
                                         user_create.password)))
         except IntegrityError:
+            logger.warning('User already exist')
             raise EntityAlreadyExist
+
+    async def get_and_check_user(self, user_login: UserInLogin) -> dict:
+        user = await self.database.fetch_one(
+                    select([Users]).where(Users.email == user_login.email))
+        if user is None:
+            logger.warning('User does not exist')
+            raise EntityDoesNotExist
+        if not security.verify_password(
+            user_login.password,
+            user['password_hash']
+                ):
+            logger.warning('Invalid password')
+            raise FailedCredentials
+        return dict(user)
+
+    async def get_access_token(self, user_login: UserInLogin):
+        user = await self.get_and_check_user(user_login)
+        return create_access_token_for_user(user)
+
+    async def get_user_by_email(self, email: str) -> dict:
+        user = await self.database.fetch_one(
+                    select([Users]).where(Users.email == email))
+        if user is None:
+            logger.warning('User does not exist')
+            raise EntityDoesNotExist
+        return dict(user)
