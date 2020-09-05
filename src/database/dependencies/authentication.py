1023dff1d59460bf71ff70ef3013badb384091e0
Lysenko Viktor <lysenko_va@groupbwt.com>
2020-09-05 23:20:23 +0300

    add error handling

--- src/database/dependencies/authentication.py
+++ src/database/dependencies/authentication.py
@@ -3,6 +3,7 @@ from typing import Optional
 from fastapi import Cookie, Depends, HTTPException, status
 
 from .database import get_repository
+from .errors import InvalidToken
 from database.repositories import UserRepository
 from database.repositories.errors import EntityDoesNotExist
 from services import get_email_from_token
@@ -13,9 +14,11 @@ async def get_current_user(
     token: Optional[str] = Cookie(None, alias='access_token')
 ) -> dict:
     try:
+        if token is None:
+            raise InvalidToken
         email = get_email_from_token(token)
         user = await users_repo.get_user_by_email(email)
-    except(ValueError, EntityDoesNotExist):
+    except(ValueError, EntityDoesNotExist, InvalidToken):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Something happened to the token. Repeat authorization."
