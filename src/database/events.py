309f66e7a5dc9738563ff3991a8b963e7a15821a
Lysenko Viktor <lysenko_va@groupbwt.com>
2020-09-06 12:19:47 +0300

    add logging

--- src/database/events.py
+++ src/database/events.py
@@ -1,5 +1,6 @@
 from typing import Callable
 
+from loguru import logger
 from databases import Database
 from fastapi import FastAPI
 
@@ -9,6 +10,7 @@ from resources import mysql_connection_string
 def connect_to_db(app: FastAPI) -> Callable:
 
     async def start_app() -> None:
+        logger.info('Connect to db')
         database = Database(mysql_connection_string())
         app.state.db = database
         await database.connect()
@@ -19,6 +21,7 @@ def connect_to_db(app: FastAPI) -> Callable:
 def close_db_connection(app: FastAPI) -> Callable:
 
     async def stop_app() -> None:
+        logger.info('Disconnect to db')
         await app.state.db.disconnect()
 
     return stop_app
