7b1f9b2b4dd6f03fb4cabd45763d8790355340fb
CartoonM <newfuturama@outlook.com>
2020-09-13 22:37:45 +0300

    add NoteInUpdate pydantic model

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -2,3 +2,4 @@ from .user_in_create import UserInCreate
 from .user_in_login import UserInLogin
 from .jwt import JWTMeta, JWTUser
 from .note_in_create import NoteInCreate
+from .note_in_update import NoteInUpdate
