f4b6ace6d57eaa441d8ae397a2a4f5d5ff752cbe
CartoonM <newfuturama@outlook.com>
2020-09-14 11:00:15 +0300

    change user variable type

--- src/database/repositories/user_repository.py
+++ src/database/repositories/user_repository.py
@@ -5,7 +5,7 @@ from pymysql.err import IntegrityError
 from .base_repository import BaseRepository
 from .errors import EntityAlreadyExist, EntityDoesNotExist, FailedCredentials
 from database.models import Users
-from schemas import UserInCreate, UserInLogin
+from schemas import UserInCreate, UserInLogin, UserInDb
 from services import security, create_access_token_for_user
 
 
@@ -24,28 +24,24 @@ class UserRepository(BaseRepository):
             logger.warning('User already exist')
             raise EntityAlreadyExist
 
-    async def get_and_check_user(self, user_login: UserInLogin) -> dict:
-        user = await self.database.fetch_one(
-                    select([Users]).where(Users.email == user_login.email))
-        if user is None:
-            logger.warning('User does not exist')
-            raise EntityDoesNotExist
+    async def get_and_check_user(self, user_login: UserInLogin) -> UserInDb:
+        user = await self.get_user_by_email(user_login.email)
         if not await security.verify_password(
             user_login.password,
-            user['password_hash']
+            user.password_hash
                 ):
             logger.warning('Invalid password')
             raise FailedCredentials
-        return dict(user)
+        return user
 
-    async def get_access_token(self, user_login: UserInLogin):
+    async def get_access_token(self, user_login: UserInLogin) -> str:
         user = await self.get_and_check_user(user_login)
         return create_access_token_for_user(user)
 
-    async def get_user_by_email(self, email: str) -> dict:
+    async def get_user_by_email(self, email: str) -> UserInDb:
         user = await self.database.fetch_one(
                     select([Users]).where(Users.email == email))
         if user is None:
             logger.warning('User does not exist')
             raise EntityDoesNotExist
-        return dict(user)
+        return UserInDb(**dict(user))
