4630f6538199b3c774650cc9ebfa3612a3ef3336
CartoonM <newfuturama@outlook.com>
2020-09-15 17:19:57 +0300

    add constants

--- src/resources/constants.py
+++ src/resources/constants.py
@@ -9,3 +9,6 @@ SECRET_KEY = getenv("SECRET_KEY", "test_secret_key")
 JWT_SUBJECT = "access"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # one week
+
+MIN_PASSWORD_LENGTH = int(getenv("MIN_PASSWORD_LENGTH", 5))
+MAX_PASSWORD_LENGTH = int(getenv("MAX_PASSWORD_LENGTH", 64))
