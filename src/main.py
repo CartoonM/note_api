3469c76aca0d322cf1b8ed1bf15ac47e71976fcf
CartoonM <newfuturama@outlook.com>
2021-01-15 14:34:34 +0200

    add cors headers

--- src/main.py
+++ src/main.py
@@ -5,14 +5,24 @@ from database import connect_to_db, close_db_connection
 from routers import authentication, notes
 
 
+origins = [
+    "http://localhost:3000"
+]
+
+headers = [
+    "auth-token"
+]
+
+
 def get_application() -> FastAPI:
     application = FastAPI()
     application.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
-        allow_headers=["*"]
+        allow_headers=["*"],
+        expose_headers=headers
     )
     application.add_event_handler("startup", connect_to_db(application))
     application.add_event_handler("shutdown", close_db_connection(application))
