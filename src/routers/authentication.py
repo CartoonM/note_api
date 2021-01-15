629e76c611b087012797c3efad77bb387540d655
CartoonM <newfuturama@outlook.com>
2021-01-15 14:35:28 +0200

    del unused import

--- src/routers/authentication.py
+++ src/routers/authentication.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Body, Depends, HTTPException, Response, status
 
 from schemas import UserInCreate, UserInLogin
-from resources.constants import ACCESS_TOKEN_EXPIRE_MINUTES
 from database.dependencies import get_repository
 from database.repositories import UserRepository
 from database.repositories.errors import (
